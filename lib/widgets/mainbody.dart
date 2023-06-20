35643c334bcc025d8760384aa750ef846c530148
Jheezzy <horlajidey.olajide3@gmail.com>
2023-06-20 13:34:03 +0100

    modified

--- lib/widgets/mainbody.dart
+++ lib/widgets/mainbody.dart
@@ -7,12 +7,26 @@ class MainBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: BoxDecoration(
-            gradient: LinearGradient(
-          colors: [],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        )),
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 78, 13, 151),
+              Color.fromARGB(255, 107, 15, 168),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Column(
+          children: [
+            Image.asset('quiz-logo.png'),
+            const Text('Learn Flutter the fun way!'),
+            OutlinedButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.arrow_right_alt),
+                label: const Text('Start Quiz'))
+          ],
+        ),
       ),
     );
   }
