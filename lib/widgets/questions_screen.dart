cdbfc00851fc0c0c75267a35b4787a589ad6c4bd
Jheezzy <horlajidey.olajide3@gmail.com>
2023-06-20 15:28:48 +0100

    modified

--- lib/widgets/questions_screen.dart
+++ lib/widgets/questions_screen.dart
@@ -1,14 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
+import 'answer_button.dart';
 
 class QuestionsScreen extends StatelessWidget {
   const QuestionsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var currentQuestion = questions[0];
+    var myMap = [
+      {'question': 'question', 'answer': 'answer'}
+    ];
+
     return Center(
-      child: Text(
-        'QUESTIONS SCREEN',
-        style: TextStyle(color: Colors.white, fontSize: 30),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            currentQuestion.text,
+            style: TextStyle(color: Colors.white),
+          ),
+          SizedBox(height: 30),
+          ...currentQuestion.answer.map((answer) {
+            return AnswerButton(
+              answerText: answer,
+              onTap: () {},
+            );
+          })
+        ],
       ),
     );
   }
