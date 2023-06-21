ea7f43e6b5a044dd22630765e0d4c9727190d4a9
Jheezzy <horlajidey.olajide3@gmail.com>
2023-06-21 08:21:06 +0100

    modified

--- lib/widgets/answer_button.dart
+++ lib/widgets/answer_button.dart
@@ -2,18 +2,18 @@ import 'package:flutter/material.dart';
 
 class AnswerButton extends StatelessWidget {
   const AnswerButton(
-      {super.key, required this.answerText, required this.onTap});
+      {super.key, required this.answerText, required this.onPressed});
 
   final String answerText;
-  final void Function() onTap;
+  final VoidCallback onPressed;
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
         style: ElevatedButton.styleFrom(
-            padding: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
-            backgroundColor: Color.fromARGB(255, 33, 1, 95),
-            shape: StadiumBorder()),
-        onPressed: onTap,
+            padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+            backgroundColor: const Color.fromARGB(255, 33, 1, 95),
+            shape: const StadiumBorder()),
+        onPressed: onPressed,
         child: Text(answerText));
   }
 }
--- lib/widgets/questions_screen.dart
+++ lib/widgets/questions_screen.dart
@@ -2,32 +2,75 @@ import 'package:flutter/material.dart';
 import 'package:quiz_app/data/questions.dart';
 import 'answer_button.dart';
 
-class QuestionsScreen extends StatelessWidget {
+class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
 
   @override
+  State<QuestionsScreen> createState() => _QuestionsScreenState();
+}
+
+class _QuestionsScreenState extends State<QuestionsScreen> {
+  var currentIndex = 0;
+  void changeQuestion() {
+    setState(() {
+      currentIndex += 1;
+    });
+    // print('changed');
+  }
+
+  @override
   Widget build(BuildContext context) {
-    var currentQuestion = questions[0];
+    // var currentQuestion = ;
+    // for (var i = 0; i < count; i++) {
+
+    // }
     var myMap = [
-      {'question': 'question', 'answer': 'answer'}
+      {
+        'question1': 'What are the main building blocks of Flutter UIs?',
+        'answer1': 'Widgets'
+      },
+      {
+        'question2': 'How are Flutter UIs built?',
+        'answer2': 'By combining widgets in code'
+      },
+      {
+        'question3': 'What\'s the purpose of a StatefulWidget?',
+        'answer3': 'Update UI as data changes'
+      },
+      {
+        'question4':
+            'Which widget should you try to use more often: StatelessWidget or StatefulWidget?',
+        'answer4': 'StatelessWidget'
+      },
+      {
+        'question5': 'What happens if you change data in a StatelessWidget?',
+        'answer5': 'The UI is not updated'
+      },
+      {
+        'question6': 'How should you update data inside of StatefulWidgets?',
+        'answer6': 'By calling setState()'
+      },
     ];
 
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            currentQuestion.text,
-            style: TextStyle(color: Colors.white),
-          ),
-          SizedBox(height: 30),
-          ...currentQuestion.answer.map((answer) {
-            return AnswerButton(
-              answerText: answer,
-              onTap: () {},
-            );
-          })
-        ],
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 40),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              questions[currentIndex].text,
+              textAlign: TextAlign.center,
+              style: const TextStyle(color: Colors.white),
+            ),
+            const SizedBox(height: 30),
+            ...questions[currentIndex].answer.map((answer) {
+              return AnswerButton(
+                  answerText: answer, onPressed: changeQuestion);
+            })
+          ],
+        ),
       ),
     );
   }
