f819bd66672a3c5cf6c49644a11da7f22f3c6ae9
Jheezzy <horlajidey.olajide3@gmail.com>
2023-06-29 01:29:58 +0100

    modified

--- lib/widgets/questions_screen.dart
+++ lib/widgets/questions_screen.dart
@@ -24,33 +24,33 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
     // for (var i = 0; i < count; i++) {
 
     // }
-    var myMap = [
-      {
-        'question1': 'What are the main building blocks of Flutter UIs?',
-        'answer1': 'Widgets'
-      },
-      {
-        'question2': 'How are Flutter UIs built?',
-        'answer2': 'By combining widgets in code'
-      },
-      {
-        'question3': 'What\'s the purpose of a StatefulWidget?',
-        'answer3': 'Update UI as data changes'
-      },
-      {
-        'question4':
-            'Which widget should you try to use more often: StatelessWidget or StatefulWidget?',
-        'answer4': 'StatelessWidget'
-      },
-      {
-        'question5': 'What happens if you change data in a StatelessWidget?',
-        'answer5': 'The UI is not updated'
-      },
-      {
-        'question6': 'How should you update data inside of StatefulWidgets?',
-        'answer6': 'By calling setState()'
-      },
-    ];
+    // var myMap = [
+    //   {
+    //     'question1': 'What are the main building blocks of Flutter UIs?',
+    //     'answer1': 'Widgets'
+    //   },
+    //   {
+    //     'question2': 'How are Flutter UIs built?',
+    //     'answer2': 'By combining widgets in code'
+    //   },
+    //   {
+    //     'question3': 'What\'s the purpose of a StatefulWidget?',
+    //     'answer3': 'Update UI as data changes'
+    //   },
+    //   {
+    //     'question4':
+    //         'Which widget should you try to use more often: StatelessWidget or StatefulWidget?',
+    //     'answer4': 'StatelessWidget'
+    //   },
+    //   {
+    //     'question5': 'What happens if you change data in a StatelessWidget?',
+    //     'answer5': 'The UI is not updated'
+    //   },
+    //   {
+    //     'question6': 'How should you update data inside of StatefulWidgets?',
+    //     'answer6': 'By calling setState()'
+    //   },
+    // ];
 
     return Container(
       margin: EdgeInsets.symmetric(horizontal: 40),
